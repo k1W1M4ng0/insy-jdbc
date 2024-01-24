889c0c45afc21ee4380995d11533bcae891a8af8
Simon Gao <simon.shubin@gmail.com>
2024-01-24 11:21:29 +0100

    ✨ Implemented place order

--- src/main/java/insy/Server.java
+++ src/main/java/insy/Server.java
@@ -181,8 +181,11 @@ GROUP BY orders.id, clients.id;
     class PlaceOrderHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
+            Session session = Server.factory.openSession();
+            session.beginTransaction();
 
-            Connection conn = setupDB();
+            System.out.println("received request for place");
+
             Map <String,String> params  = queryToMap(t.getRequestURI().getQuery());
 
             int client_id = Integer.parseInt(params.get("client_id"));
@@ -190,93 +193,183 @@ GROUP BY orders.id, clients.id;
             String response = "";
             int order_id = 1;
             int order_lines_id = 1;
-            try (
-                    Statement st = conn.createStatement();
-                    ResultSet maxIdSet = st.executeQuery("SELECT MAX(id) FROM orders;");
-                    Statement st2 = conn.createStatement();
-                    ResultSet maxOrderLinesIdSet = st2.executeQuery("SELECT MAX(id) FROM order_lines;");
-                ){
 
-                // if there are already orders
-                if(maxIdSet.next()) {
-                    // set the order id to the max + 1
-                    order_id = maxIdSet.getInt(1) + 1;
-                }
-                if(maxOrderLinesIdSet.next()) {
+            Object maxIdQuery = session.createNativeQuery("select max(id) from orders;", Object.class)
+                .getSingleResultOrNull();
+            Object maxOrderLineQuery = session.createNativeQuery("select max(id) from order_lines;", Object.class)
+                .getSingleResultOrNull();
 
-                    order_lines_id = maxOrderLinesIdSet.getInt(1) + 1;
-                }
+            // get the client
+            Client client = session.get(Client.class, client_id);
+            // Client client = session.createSelectionQuery("from Client where id = ?1", Client.class)
+            //     .setParameter(1, client_id)
+            //     .getSingleResultOrNull();
 
-                // Create a new order with this id for client client_id
-                JSONObject orderObject = new JSONObject();
-                // set order id
-                orderObject.put("id", order_id);
-                // set client id
-                orderObject.put("client_id", client_id);
+            // if there are already orders
+            if(maxIdQuery != null) {
+                // set the order id to the max + 1
+                order_id = (Integer)maxIdQuery + 1;
+            }
+            if(maxOrderLineQuery != null) {
 
-                // insert a new order
-                String insertOrderQuery = String.format(
-                        "INSERT INTO orders (id, client_id) VALUES (%d, %d);",
-                        order_id,
-                        client_id
-                        );
-                st.executeUpdate(insertOrderQuery);
+                order_lines_id = (Integer)maxOrderLineQuery + 1;
+            }
 
+            System.out.println("orderid: " + order_id);
 
-                for (int i = 1; i <= (params.size()-1) / 2; ++i ){
-                    int article_id = Integer.parseInt(params.get("article_id_"+i));
-                    int amount = Integer.parseInt(params.get("amount_"+i));
+            // Create a new order with this id for client client_id
+            JSONObject orderObject = new JSONObject();
+            Order order = new Order();
+            order.setId(order_id);
+            order.setClient(client);
+            System.out.println(order.getId() + ", " + order.getClient().getId());
+            // set order id
+            orderObject.put("id", order_id);
+            // set client id
+            orderObject.put("client_id", client_id);
 
+            // insert a new order
+            String insertOrderQuery = String.format(
+                    "INSERT INTO orders (id, client_id) VALUES (%d, %d);",
+                    order_id,
+                    client_id
+                    );
+            // System.out.println("now persist");
+            // try {
+
+                // doesnt work
+                // session.merge(order);
+                // session.merge(order.getClient());
+                // session.persist("Order", order);
+                // session.persist(order);
+                
+                session.createNativeMutationQuery(insertOrderQuery).executeUpdate();
+                session.getTransaction().commit();
+
+            // }
+            // catch(Exception ex) {
+            //     ex.printStackTrace();
+            // }
+            // System.out.println("persisted");
+           try { 
+
+            for (int i = 1; i <= (params.size()-1) / 2; ++i ){
+                int article_id = Integer.parseInt(params.get("article_id_"+i));
+                int amount = Integer.parseInt(params.get("amount_"+i));
+                System.out.println("handling article id " + article_id);
 
                 // Get the available amount for article article_id
-                    int available = 0;
-                    String query = String.format("SELECT amount FROM articles WHERE id = %d;", article_id);
-                    if(query.indexOf('"') != -1) {
-                        throw new IllegalArgumentException("no \" allowed");
-                    }
-                    try(ResultSet availableAmountSet = st.executeQuery(query)) {
-                        if(availableAmountSet.next()) {
-                            available = availableAmountSet.getInt(1);
-                        }
-                    } // catch in the outer catch block
+                int available = 0;
+                Article article = session.get(Article.class, article_id);
+                available = article.getAmount();
+                System.out.println("got article: " + article.getId() + ", available: " + available);
 
 
-                    if (available < amount)
-                        throw new IllegalArgumentException(String.format("Not enough items of article #%d available", article_id));
+                if (available < amount)
+                    throw new IllegalArgumentException(String.format("Not enough items of article #%d available", article_id));
 
-                    // Decrease the available amount for article article_id by amount
+                // Decrease the available amount for article article_id by amount
 
-                    String updateArticleAmountQuery = String.format(
-                            "UPDATE articles SET amount = amount - %d WHERE id = %d;",
-                            amount,
-                            article_id
-                            );
-                    st.executeUpdate(updateArticleAmountQuery);
+                String updateArticleAmountQuery = String.format(
+                        "UPDATE Article SET amount = amount - %d WHERE id = %d",
+                        amount,
+                        article_id
+                        );
+                System.out.println("decreasing amount for article");
+                session.beginTransaction();
+                session.createMutationQuery(updateArticleAmountQuery).executeUpdate();
+                session.getTransaction().commit();
 
-                    // Insert new order line
-                    String insertOrderLineQuery = String.format(
-                            "INSERT INTO order_lines VALUES(%d, %d, %d, %d);",
-                            order_lines_id++,
-                            article_id,
-                            order_id,
-                            amount
-                            );
-                    st.executeUpdate(insertOrderLineQuery);
-
-                
-                }
-
-                response = String.format("{\"order_id\": %d}", order_id);
-            } 
-            catch(SQLException ex) {
-                response = String.format("{\"error\":\"%s\"}", ex.toString());
-                ex.printStackTrace();
+                // Insert new order line
+                OrderLine orderLine = new OrderLine();
+                orderLine.setId(order_lines_id++);
+                orderLine.setArticle(article);
+                orderLine.setOrder(order);
+                orderLine.setAmount(amount);
+                String insertOrderLineQuery = String.format(
+                        "INSERT INTO order_lines VALUES(%d, %d, %d, %d);",
+                        order_lines_id++,
+                        article_id,
+                        order_id,
+                        amount
+                        );
+                System.out.println("persisting order line");
+                // session.persist(orderLine);
+                session.beginTransaction();
+                session.createNativeMutationQuery(insertOrderLineQuery).executeUpdate();
+                session.getTransaction().commit();
             }
-            catch (IllegalArgumentException iae) {
-                response = String.format("{\"error\":\"%s\"}", iae.getMessage());
-            }
+           }catch( Throwable th) {
+               th.printStackTrace();
+           }
 
+            response = String.format("{\"order_id\": %d}", order_id);
             answerRequest(t, response);
+
+            // try (
+            //         Statement st = conn.createStatement();
+            //         ResultSet maxIdSet = st.executeQuery("SELECT MAX(id) FROM orders;");
+            //         Statement st2 = conn.createStatement();
+            //         ResultSet maxOrderLinesIdSet = st2.executeQuery("SELECT MAX(id) FROM order_lines;");
+            //     ){
+            //
+            //
+            //
+            //
+            //     for (int i = 1; i <= (params.size()-1) / 2; ++i ){
+            //         int article_id = Integer.parseInt(params.get("article_id_"+i));
+            //         int amount = Integer.parseInt(params.get("amount_"+i));
+            //
+            //
+            //     // Get the available amount for article article_id
+            //         int available = 0;
+            //         String query = String.format("SELECT amount FROM articles WHERE id = %d;", article_id);
+            //         if(query.indexOf('"') != -1) {
+            //             throw new IllegalArgumentException("no \" allowed");
+            //         }
+            //         try(ResultSet availableAmountSet = st.executeQuery(query)) {
+            //             if(availableAmountSet.next()) {
+            //                 available = availableAmountSet.getInt(1);
+            //             }
+            //         } // catch in the outer catch block
+            //
+            //
+            //         if (available < amount)
+            //             throw new IllegalArgumentException(String.format("Not enough items of article #%d available", article_id));
+            //
+            //         // Decrease the available amount for article article_id by amount
+            //
+            //         String updateArticleAmountQuery = String.format(
+            //                 "UPDATE articles SET amount = amount - %d WHERE id = %d;",
+            //                 amount,
+            //                 article_id
+            //                 );
+            //         st.executeUpdate(updateArticleAmountQuery);
+            //
+            //         // Insert new order line
+            //         String insertOrderLineQuery = String.format(
+            //                 "INSERT INTO order_lines VALUES(%d, %d, %d, %d);",
+            //                 order_lines_id++,
+            //                 article_id,
+            //                 order_id,
+            //                 amount
+            //                 );
+            //         st.executeUpdate(insertOrderLineQuery);
+            //
+            //     
+            //     }
+            //
+            //     response = String.format("{\"order_id\": %d}", order_id);
+            // } 
+            // catch(SQLException ex) {
+            //     response = String.format("{\"error\":\"%s\"}", ex.toString());
+            //     ex.printStackTrace();
+            // }
+            // catch (IllegalArgumentException iae) {
+            //     response = String.format("{\"error\":\"%s\"}", iae.getMessage());
+            // }
+            //
+            // answerRequest(t, response);
 
 
         }
